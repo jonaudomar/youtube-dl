2d6bce7085e319679ed84558010dcc5f2b05a3b8
Jonathan D <jdelrue-web@proton.me>
2025-12-17 17:39:47 +0100

    update

--- app.py
+++ app.py
@@ -2,17 +2,24 @@ import streamlit as st
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
 
-st.title("Hello world")
+st.title("Téléchargement depuis YouTube")
 
 url = st.text_input("Copiez-coller le lien de la vidéo", "https://www.youtube.com/watch?v=iyLdoQGBchQ")
 
-if st.button("Télécharger MP4", type="primary"):
+if url:
     yt = YouTube(url, on_progress_callback=on_progress)
-    st.write("Téléchargement de :", {yt.title})
-    ys = yt.streams.get_highest_resolution()
-    ys.download()
+    st.text(yt.title)
+    st.video(url)
 
-if st.button("Télécharger son uniquement", type="secondary"):
-    yt = YouTube(url, on_progress_callback=on_progress)
-    ys = yt.streams.get_audio_only()
-    ys.download()
+    if st.button("Télécharger MP4", type="primary"):
+        st.write("Téléchargement de :", yt.title)
+        ys = yt.streams.get_highest_resolution()
+        ys.download(output_path="files/")
+        st.success("Téléchargement terminé !")
+
+    if st.button("Télécharger son uniquement", type="secondary"):
+        ys = yt.streams.get_audio_only()
+        ys.download(output_path="files/")
+        st.success("Téléchargement terminé !")
+else:
+    st.markdown(":orange-badge[⚠️ Entrer un lien ci-dessus]")
